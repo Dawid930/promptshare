6403f89dfc46ef914c9ac426b913052f29dfc59c
Dawid930 <davidnyikes@gmail.com>
2023-11-13 17:02:07 +0100

    feat: implement MongoDB, authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,17 +8,17 @@ import {signIn, signOut, useSession, getProviders} from "next-auth/react"
 
 
 const Nav = () => {
-    const isUserLoggedIn = true
+    const {data: session} = useSession()
 
 const [providers, setProviders] = useState(null)   
 const [toggleDropdown, setToggleDropdown] = useState(false)
 
 useEffect(() => {
-    const setProvidersList = async () => {
+    const setUpProviders = async () => {
         const response = await getProviders()
         setProviders(response)
     }
-    setProviders()
+    setUpProviders()
 }, [])
 
 
@@ -35,81 +35,82 @@ useEffect(() => {
                 Promptshare
             </p>
         </Link>
-            {/* Desktop Navigation */}
-            <div className="sm:flex hidden">
-                {isUserLoggedIn ? (
-                    <div className="flex gap-3 md:gap-5">
-                        <Link href="/create-prompt" className="black_btn">
-                            Create Post
-                        </Link>
-                        <button type="button" onClick={signOut} className="outline_btn">
-                            Sign Out
-                        </button>
+        
+        {/* Desktop Navigation */}
+        <div className="sm:flex hidden">
+            {session?.user ? (
+                <div className="flex gap-3 md:gap-5">
+                    <Link href="/create-prompt" className="black_btn">
+                        Create Post
+                    </Link>
+                    <button type="button" onClick={signOut} className="outline_btn">
+                        Sign Out
+                    </button>
 
-                        <Link href='/profile'>
-                             <Image 
-                                src="/assets/images/logo.svg"
-                                width={37}
-                                height={37}
-                                className="rounded-full"
-                                alt="profile"
-                             />
-                        </Link>
-                    </div>
-                    ) : (
-                        <>
-                        {providers && Object.values(providers).map((provider) => (
-                                <button key={provider.name} 
-                                type="button" 
-                                onClick={() => signIn(provider.id)}
-                                className="black_btn"
-                                >
-                                Sign In
-                                </button>
-                        ))}
-                        </>
-                    )}
-            </div>
-            {/* Mobile Navigation */}
-            <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
-                    <div>
-                        <Image 
-                        src="/assets/images/logo.svg"
-                        width={37}
-                        height={37}
-                        className="rounded-full"
-                        alt="profile"
-                        onClick={() => setToggleDropdown((prev) => !prev)} //is better then setToggleDropdown(!toggleDropdown) because it will always be the opposite of what it was before
-                        />
-                        {toggleDropdown && (
-                            <div className="dropdown">
-                                <Link 
-                                href='profile'
-                                className="dropdown_link"
-                                onClick={() => setToggleDropdown(false)}
-                                >
-                                    My Profile
-                                </Link>
-                                <Link 
-                                href='/create-prompt'
-                                className="dropdown_link"
-                                onClick={() => setToggleDropdown(false)}
-                                >
-                                    Create Prompt
-                                </Link>
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        setToggleDropdown(false)
-                                        signOut()}}
-                                    className="mt-5 w-full black_btn"
-                                >
-                                    Sign Out
-                                </button>
-                                </div>
-                            )}
-                    </div>
+                    <Link href='/profile'>
+                            <Image 
+                            src={session?.user.image}
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="profile"
+                            />
+                    </Link>
+                </div>
+                ) : (
+                    <>
+                    {providers && Object.values(providers).map((provider) => (
+                            <button key={provider.name} 
+                            type="button" 
+                            onClick={() => signIn(provider.id)}
+                            className="black_btn"
+                            >
+                            Sign In
+                            </button>
+                    ))}
+                    </>
+                )}
+        </div>
+        {/* Mobile Navigation */}
+        <div className="sm:hidden flex relative">
+            {session?.user ? (
+                <div>
+                    <Image 
+                    src={session?.user.image}
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="profile"
+                    onClick={() => setToggleDropdown((prev) => !prev)} //is better then setToggleDropdown(!toggleDropdown) because it will always be the opposite of what it was before
+                    />
+                    {toggleDropdown && (
+                        <div className="dropdown">
+                            <Link 
+                            href='profile'
+                            className="dropdown_link"
+                            onClick={() => setToggleDropdown(false)}
+                            >
+                                My Profile
+                            </Link>
+                            <Link 
+                            href='/create-prompt'
+                            className="dropdown_link"
+                            onClick={() => setToggleDropdown(false)}
+                            >
+                                Create Prompt
+                            </Link>
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setToggleDropdown(false)
+                                    signOut()}}
+                                className="mt-5 w-full black_btn"
+                            >
+                                Sign Out
+                            </button>
+                            </div>
+                        )}
+                </div>
                 ): (
                     <>
                     {providers && Object.values(providers).map((provider) => (
@@ -123,7 +124,7 @@ useEffect(() => {
                         ))}
                     </>
                 )}
-            </div>
+        </div>
     </nav>
   )
 }
