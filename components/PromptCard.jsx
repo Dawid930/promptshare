7c107a53304673fd7eedcbf51efc7a2554ea9261
Dawid930 <davidnyikes@gmail.com>
2023-11-20 16:06:58 +0100

    add user pages

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -3,12 +3,13 @@
 import {useState} from 'react'
 import Image from 'next/image';
 import { useSession } from 'next-auth/react'
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
 
   const {data: session} = useSession()
   const pathName = usePathname()
+  const router = useRouter()
 
   const [copied, setCopied] = useState('')
 
@@ -18,28 +19,33 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     setTimeout(() =>  setCopied(''), 3000)
   }
 
+  const handleProfileClick = () => {
+    if(post.creator._id === session?.user.id) return router.push('/profile')
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
+  }
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
-        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
-        <Image
-          src={post.creator.image}
-          alt='user_image'
-          width={40}
-          height={40}
-          className='rounded-full object-contain'
-        />
+        <div onClick={handleProfileClick} className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+          <Image
+            src={post.creator.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+            />
 
-        <div className='flex flex-col'>
-          <h3 className='font-satoshi font-semibold text-gray-900'>
-            {post.creator.username}
-          </h3>
-          <p className='font-inter text-sm text-gray-500'>
-            {post.creator.email}
-          </p>
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-gray-900'>
+              {post.creator.username}
+            </h3>
+            <p className='font-inter text-sm text-gray-500'>
+              {post.creator.email}
+            </p>
 
-        </div>
-
+          </div>
         </div>
 
         <div className='copy_btn' onClick={handleCopy}>
@@ -80,5 +86,6 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     </div>
   )
 }
+
 
 export default PromptCard
