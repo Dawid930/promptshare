8cdb2e51e7b3fe2513df44c0206541fce869daf3
Dawid930 <davidnyikes@gmail.com>
2023-11-28 18:33:53 +0100

    deploy commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,9 +27,6 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`)
   }
 
-  const asdf = () => {
-    sdfs
-  }
   const handleDelete = async (post) => {
     const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
 
